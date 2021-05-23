f3f59b60ebcc7e187dc35509a86c75b55627907e
Dawid Kocik <kocik.dawid@wp.pl>
2021-05-23 10:38:35 +0200

    Module 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -47,6 +48,11 @@ public class StreamMain {
 
         poemBeautifier.toUpperCase( "Child "," Dad "," Mom",(a,b,c) -> "Family = "+ "[" +a+b+c + "]");
         poemBeautifier.toUpperCase( " ABC "," DEF "," GHI ",(a,b,c) -> "Alphabet = "+ a+b+c);
+
+        System.out.println();
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
 
     }
