1de2df12c10ddba28696e25b81425671ebcc27d9
Dawid Kocik <kocik.dawid@wp.pl>
2021-05-31 17:41:15 +0200

    Module 8.3

--- kodilla-exception/src/test/java/com/kodilla/exception/io/ExceptionHandling.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/ExceptionHandling.java
@@ -1,4 +1,18 @@
 package com.kodilla.exception.io;
 
 public class ExceptionHandling {
+    public static void main(String[] args){
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            System.out.println("probablyIWillThrowException result = " + secondChallenge.probablyIWillThrowException(2.0,3.0));
+        }
+        catch (Exception e) {
+            System.out.println("Don't probablyIWillThrowException \n Error occurred" + e);
+        }
+        finally {
+                System.out.println("Done !");
+        }
+    }
 }
