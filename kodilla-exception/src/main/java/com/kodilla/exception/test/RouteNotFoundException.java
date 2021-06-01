304a238988512acfa69f068b16a5de592f939ab8
Dawid Kocik <kocik.dawid@wp.pl>
2021-06-01 23:45:49 +0200

    Module 8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundException.java
@@ -3,9 +3,10 @@ package com.kodilla.exception.test;
 public class RouteNotFoundException extends Exception{
     public RouteNotFoundException(String message) {
         super(message);
-
     }
 
-
-
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
