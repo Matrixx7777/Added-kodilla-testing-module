8ed87c3c4b617a357f25ed97dd3f90a2c2a1995b
Dawid Kocik <kocik.dawid@wp.pl>
2021-12-10 16:44:02 +0100

    Module 31.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -9,14 +9,14 @@ import org.openqa.selenium.support.ui.Select;
 public class FacebookTestingApp {
 
     public static final String ACCEPT_COOKIES_BUTTON_X_PATH = "//*[@data-testid='cookie-policy-dialog-accept-button']";
-    public static final String CREAT_NEW_ACCOUNT = "//*[@data-testid='open-registration-form-button']";
-    public static final String SELECT_NAME = "//*[@id=\"u_4_b_Oy\" ]";
-    public static final String SELECT_SURNAME = "//*[@id=\"u_8_d_Hh\"]";
-    public static final String SELECT_PHONE_NUMBERS = "//*[@id=\"u_8_g_pT\"]";
-    public static final String SELECT_PASSWORD = "//*[@id=\"password_step_input\"]";
-    public static final String SELECT_BIRTHDAY_DAY = "//*select[@id=\"day\"]";
-    public static final String SELECT_BIRTHDAY_MONTH = "//*[@id=\"month\"]";
-    public static final String SELECT_BIRTHDAY_YEAR = "//*[@id=\"year\"]";
+    public static final String CREAT_NEW_ACCOUNT = "/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a";
+    public static final String SELECT_NAME = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[1]/div/input";
+    public static final String SELECT_SURNAME = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input";
+    public static final String SELECT_PHONE_NUMBERS = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input";
+    public static final String SELECT_PASSWORD = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[4]/div/div[1]/input";
+    public static final String SELECT_BIRTHDAY_DAY = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[1]";
+    public static final String SELECT_BIRTHDAY_MONTH = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[2]";
+    public static final String SELECT_BIRTHDAY_YEAR = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[3]";
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -63,18 +63,18 @@ public class FacebookTestingApp {
 
         WebElement selectComboDay = driver.findElement(By.xpath(SELECT_BIRTHDAY_DAY));
         Select selectBoardDay = new Select(selectComboDay);
-        selectBoardDay.selectByIndex(16);
+        selectBoardDay.selectByIndex(17);
 
-        Thread.sleep(2000L);
+        Thread.sleep(1000L);
 
         WebElement selectComboMonth = driver.findElement(By.xpath(SELECT_BIRTHDAY_MONTH));
         Select selectBoardMonth = new Select(selectComboMonth);
         selectBoardMonth.selectByIndex(5);
 
-        Thread.sleep(2000L);
+        Thread.sleep(1000L);
 
         WebElement selectComboYear = driver.findElement(By.xpath(SELECT_BIRTHDAY_YEAR));
         Select selectBoardYear = new Select(selectComboYear);
-        selectBoardYear.selectByIndex(1995);
+        selectBoardYear.selectByIndex(2000);
     }
 }
